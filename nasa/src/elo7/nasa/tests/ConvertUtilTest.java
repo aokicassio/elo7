d418dcec14cfe6bbc28b8c80c6959133659cd27c
aokicassio <cassiolaoki@gmail.com>
2016-01-03 20:13:45 -0200

    Inclusao de metodos de teste

--- nasa/src/elo7/nasa/tests/ConvertUtilTest.java
+++ nasa/src/elo7/nasa/tests/ConvertUtilTest.java
@@ -1,8 +1,12 @@
 package elo7.nasa.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import elo7.nasa.entity.commons.Comando;
 import elo7.nasa.entity.commons.Direcao;
 import elo7.nasa.exception.ConvertException;
 import elo7.nasa.utils.ConvertUtil;
@@ -10,7 +14,7 @@ import elo7.nasa.utils.ConvertUtil;
 public class ConvertUtilTest {
 
 	@Test
-	public void testComandoGirarRight() throws ConvertException{
+	public void testConverterDirecao() throws ConvertException{
 		
 		Assert.assertEquals(Direcao.N, ConvertUtil.converterDirecao("N"));
 		
@@ -19,6 +23,28 @@ public class ConvertUtilTest {
 		Assert.assertEquals(Direcao.W, ConvertUtil.converterDirecao("W"));
 		
 		Assert.assertEquals(Direcao.E, ConvertUtil.converterDirecao("E"));
+
+	}
+	
+	@Test
+	public void testConverterComandosString() throws ConvertException{
+		
+		List<Comando> lista = new ArrayList<>();
+
+		
+		String s = "MLR";
+
+		lista.add(Comando.M);
+		lista.add(Comando.L);
+		lista.add(Comando.R);
+		
+		Assert.assertEquals(lista, ConvertUtil.converterComandosString(s));
+		
+		
+		s = "";
+		lista = new ArrayList<>();
+
+		Assert.assertEquals(lista, ConvertUtil.converterComandosString(s));
 
 	}
 	
